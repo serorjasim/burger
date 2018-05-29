c415ca547352fbbd31c01040c36ec86f52ccd14e
seror <seror94ab@yahoo.com>
2018-05-29 14:27:25 -0500

    creating folders and files

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+
+id INT auto_increment PRIMARY KEY NOT NULL,
+burger_name VARCHAR(50) NOT NULL,
+devoured BOOLEAN
+
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("primetime", ture),
+("the greek", true),
+("good night", false);
